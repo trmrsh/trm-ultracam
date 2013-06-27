febf52c12061058986f4f3556bc281d27cfe6ae1
Tom Marsh <t.r.marsh@warwick.ac.uk>
2013-06-27 23:02:17 +0100

    Fixed and improved
    Adjusted for latest utimer, specialised to a single run

--- scripts/utimes.py
+++ scripts/utimes.py
@@ -2,66 +2,44 @@
 
 usage = \
 """
-Computes and and stores stats on all ultracam files it can locate from the directory tree
-starting with the current directory. For each run it locates,  it prints out the times of all frames.
-
-The script specifically searches for all directories of the form YYYY-MM-DD, but you can specify
-a regular expression to narrow the search. e.g. '2010' will find all runs in directories containing
+Prints out times of an ULTRACAM run.  The script specifically searches for all
+directories of the form YYYY-MM-DD, but you can specify a regular expression
+to narrow the search. e.g. '2010' will find all runs in directories containing
 2010, as well as having the YYYY-MM-DD format.
 """
 
 # builtins
-import argparse, os, re, sys, traceback
-
-# thirdparty
-import numpy as np
+import argparse, os
 
 # mine
 from trm import ultracam
 
 parser = argparse.ArgumentParser(description=usage)
 
+# positional
+parser.add_argument('run', help='run to list times of')
+
 # optional
-parser.add_argument('--regex', '-r', help='regular expression for matching specific directories')
-parser.add_argument('--suppress', '-s', action='store_true', help='suppress all but bad output')
+parser.add_argument('-s', dest='suppress', action='store_true', help='suppress all but bad output')
 
 # OK, done with arguments.
 args = parser.parse_args()
 
-if args.regex is not None:
-    rext = re.compile(args.regex)
-else:
-    rext = None
+run = args.run
+if not os.path.isfile(run + '.xml') or not os.path.isfile(run + '.dat'):
+    print 'One or both of',run+'.xml','and',run+'.dat','does not exist.'
+    exit(1)
 
-rdir = re.compile('\d\d\d\d-\d\d-\d\d$')
-rmat = re.compile('^run\d\d\d\.xml$')
+tdat = ultracam.Rtime(run)
 
-for rpath, rnames, fnames in os.walk('.'):
-
-    # search only YYYY-MM-DD directories
-    if rdir.search(rpath) and (rext is None or rext.search(rpath)):
-        
-        fnames.sort()
-        runs  = [os.path.join(rpath, fname[:-4]) for fname in fnames if rmat.match(fname)]
-
-        print '\n\nFound',len(runs),'runs in directory = ',rpath,'\n'
-
-        for run in runs:
-
-            if not os.path.exists(run + '.dat'):
-                print run + '.dat does not exist.'
-                continue
-
-            try:
-                print 'Processing',run
-                tdat = ultracam.Rtime(run)
-
-                first = True
-                for time in tdat:
-                    if not args.suppress or (first and not time[0].good):
-                        print time
-                        first = False
-                    
-            except Exception, err:
-                print 'Encountered problem on',run
-                traceback.print_exc(file=sys.stdout)
+first = True
+for nf, time in enumerate(tdat):
+    if not args.suppress or (first and not time[0].good):
+        print 'Frame %d, mid-time = %s, GPS = %s, exposure = %7.4f, status = %s' % \
+            (nf+1,ultracam.mjd2str(time[0].mjd,True),ultracam.mjd2str(time[3]['gps'],True),\
+                 time[0].expose, 'T' if time[0].good else 'F'),
+        if not time[0].good:
+            print ', reason =',time[0].reason
+        else:
+            print
+        first = False
