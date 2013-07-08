74b8b0824bbc16ea6b4069b671c24bd55f8b9bcd
Tom Marsh <t.r.marsh@warwick.ac.uk>
2013-07-08 16:55:52 +0100

    fixed a bug and added more tests
    xbin and ybin were not being tested correctly. Started on CCD tests

--- test/test_ultracam.py
+++ test/test_ultracam.py
@@ -1,5 +1,7 @@
 """
-Tests for trm.ultracam
+Tests for trm.ultracam.
+
+Run with 'python test_ultracam.py'
 """
 
 import unittest
@@ -38,7 +40,7 @@ class TestWindow(unittest.TestCase):
     def test_xbin(self):
         self.assertEqual(self.win1.xbin, 2)
 
-    def test_xbin(self):
+    def test_ybin(self):
         self.assertEqual(self.win1.ybin, 3)
 
     def test_size(self):
@@ -76,6 +78,54 @@ class TestWindow(unittest.TestCase):
         win = self.win1 / self.win2
         self.assertEqual(win[4,5],2./3.)
 
+    def test_addc(self):
+        win = self.win1 + 10.
+        self.assertEqual(win[4,5],12.)
+
+    def test_subc(self):
+        win = self.win1 - 10.
+        self.assertEqual(win[4,5],-8.)
+
+    def test_mulc(self):
+        win = self.win1 * 10.
+        self.assertEqual(win[4,5],20.)
+
+    def test_divc(self):
+        win = self.win1 / 10.
+        self.assertEqual(win[4,5],0.2)
+
+    def test_radd(self):
+        win = 10. + self.win1
+        self.assertEqual(win[4,5],12.)
+
+class TestCCD(unittest.TestCase):
+
+    def setUp(self):
+        """
+        Create two Windows, place into CCDs
+        """
+        llx1, lly1, xbin, ybin = 12, 22, 2, 3
+        dat1  = np.zeros((10,20))
+        dat1 += 5
+        win1 = ultracam.Window(dat1, llx1, lly1, xbin, ybin) 
+
+        llx2, lly2, xbin, ybin = 120, 220, 2, 3
+        dat2  = dat1 + 10.
+        win2 = ultracam.Window(dat2, llx2, lly2, xbin, ybin) 
+
+        time = ultracam.Time(56000.2, 5., True, '')
+
+        head = ultracam.Uhead()
+        head.add_entry('Object', 'IP Peg', ultracam.ITYPE_STRING,'Name of target')
+        self.ccd = ultracam.CCD([win1,win2],time,900,800,True,head)
+
+
+    def test_index(self):
+        self.assertEqual(self.ccd[1][4,5],15.)
 
 if __name__ == '__main__':
-    unittest.main()
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestWindow)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestCCD)
+    unittest.TextTestRunner(verbosity=2).run(suite)
