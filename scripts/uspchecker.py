de2b9299395c6b54bc680f518eb948105de9679a
Tom Marsh <t.r.marsh@warwick.ac.uk>
2014-01-29 23:51:44 +0000

    uspchecker.py, uspfixer.py -- new scripts for timing problem
    Detect and correct problem with ultraspec timestamps.

---
+++ scripts/uspchecker.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+usage = \
+"""
+Checks the present working directory for runs containing the
+ULTRASPEC timing problem spotted in January 2014.
+"""
+
+import argparse, os, re, sys
+from trm import ultracam
+
+parser = argparse.ArgumentParser(description=usage)
+parser.add_argument('-s','--server',action="store_true",
+                    help="get runs from server")
+args = parser.parse_args()
+
+server = args.server
+
+rmat = re.compile('^run\d\d\d\.xml$')
+
+for rpath, rnames, fnames in os.walk('.'):
+    runs  = [fname[:-4] for fname in fnames if rmat.match(fname)]
+    runs.sort()
+    for run in runs:
+        try:
+            tdat  = ultracam.Rtime(run,server=server)
+            badFrames = []
+            for nf, time in enumerate(tdat):
+                if nf:
+                    if (not time[0].good) and \
+                       (time[0].reason == 'timestamp too early'):
+                        badFrames.append(nf+1)
+
+            if len(badFrames):
+                print run,'has timing bug in frames',badFrames
+            else:
+                print run,'is OK'
+
+
+        except:
+            print 'Failed to read ',run
