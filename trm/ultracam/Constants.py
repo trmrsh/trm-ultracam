fe4f189ed818dc61810de5cdef961deda2a0d415
Tom Marsh <t.r.marsh@warwick.ac.uk>
2014-01-30 12:41:46 +0000

    Constants.py -- added Nov 2013 TNT run to run list

--- trm/ultracam/Constants.py
+++ trm/ultracam/Constants.py
@@ -44,7 +44,7 @@ BIAS_LEVELS = {'cdd' : (
         None,
         None
         )}
-               
+
 # ULTRACAM Timing parameters from Vik
 INVERSION_DELAY = 110.          # microseconds
 HCLOCK          = 0.48          # microseconds
@@ -55,7 +55,7 @@ SWITCH_TIME     = 1.2           # microseconds
 
 USPEC_FT_ROW    = 14.4e-6       # seconds
 USPEC_FT_OFF    = 49.e-6        # seconds
-USPEC_CLR_TIME  = 0.0309516     # seconds 
+USPEC_CLR_TIME  = 0.0309516     # seconds
 
 # Run dates (for checking for spurious times).
 # Format: overall run id, start, stop.
@@ -101,6 +101,7 @@ RUN_DATES = (('2002-05','2002-05-16','2002-05-20'),
              ('2013-07','2013-07-10','2013-07-16'),
              ('2013-07','2013-07-20','2013-07-21'),
              ('2013-07','2013-07-25','2013-08-05'),
+             ('2013-11','2013-11-05','2013-11-13'),
              )
 
 RUN_TELS = {'2002-05' : 'WHT',
@@ -130,6 +131,7 @@ RUN_TELS = {'2002-05' : 'WHT',
             '2012-10' : 'WHT',
             '2013-04' : 'WHT',
             '2013-07' : 'WHT',
+            '2013-11' : 'TNT',
             }
 
 
@@ -156,7 +158,7 @@ ITYPE_USINT     = 16
 ITYPE_IVECTOR   = 17
 ITYPE_FVECTOR   = 18
 
-TNAME = {ITYPE_DOUBLE : 'double', ITYPE_INT : 'int', ITYPE_UINT : 'uint', 
+TNAME = {ITYPE_DOUBLE : 'double', ITYPE_INT : 'int', ITYPE_UINT : 'uint',
          ITYPE_FLOAT : 'float', ITYPE_STRING : 'string', ITYPE_BOOL : 'bool',
          ITYPE_DIR : 'directory', ITYPE_TIME : 'time', ITYPE_DVECTOR : 'dvector',
          ITYPE_UCHAR : 'uchar', ITYPE_USINT : 'usint', ITYPE_IVECTOR : 'ivector',
@@ -165,18 +167,18 @@ TNAME = {ITYPE_DOUBLE : 'double', ITYPE_INT : 'int', ITYPE_UINT : 'uint',
 # ucm magic number
 MAGIC           = 47561009
 
-# Bit masks needed for Meinberg GPS data. 
+# Bit masks needed for Meinberg GPS data.
 # See description in read_header.cc in pipeline for more
-PCPS_FREER            = 0x01   # DCF77 clock running on xtal, GPS receiver has not verified its position 
-PCPS_DL_ENB           = 0x02   # daylight saving enabled 
-PCPS_SYNCD            = 0x04   # clock has sync'ed at least once after pwr up 
-PCPS_DL_ANN           = 0x08   # a change in daylight saving is announced 
-PCPS_UTC              = 0x10   # a special UTC firmware is installed 
+PCPS_FREER            = 0x01   # DCF77 clock running on xtal, GPS receiver has not verified its position
+PCPS_DL_ENB           = 0x02   # daylight saving enabled
+PCPS_SYNCD            = 0x04   # clock has sync'ed at least once after pwr up
+PCPS_DL_ANN           = 0x08   # a change in daylight saving is announced
+PCPS_UTC              = 0x10   # a special UTC firmware is installed
 PCPS_LS_ANN           = 0x20   # leap second announced, (requires firmware rev. REV_PCPS_LS_ANN_...)
-PCPS_IFTM             = 0x40   # the current time was set via PC, (requires firmware rev. REV_PCPS_IFTM_...) 
+PCPS_IFTM             = 0x40   # the current time was set via PC, (requires firmware rev. REV_PCPS_IFTM_...)
 PCPS_INVT             = 0x80   # invalid time because battery was disconn'd
-PCPS_LS_ENB           = 0x0100 # current second is leap second 
-PCPS_ANT_FAIL         = 0x0200 # antenna failure 
-PCPS_UCAP_OVERRUN     = 0x2000 # events interval too short 
-PCPS_UCAP_BUFFER_FULL = 0x4000 # events read too slow 
+PCPS_LS_ENB           = 0x0100 # current second is leap second
+PCPS_ANT_FAIL         = 0x0200 # antenna failure
+PCPS_UCAP_OVERRUN     = 0x2000 # events interval too short
+PCPS_UCAP_BUFFER_FULL = 0x4000 # events read too slow
 PCPS_IO_BLOCKED       = 0x8000 # access to microprocessor blocked
