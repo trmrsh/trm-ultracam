faa7e9a16aec2cdaa225d032881d555eac61f521
Tom Marsh <t.r.marsh@warwick.ac.uk>
2014-03-13 20:54:54 +0000

    tchecker.py -- fixed bugs
    Import statements fixed amonst other things

--- scripts/tchecker.py
+++ scripts/tchecker.py
@@ -3,11 +3,13 @@
 usage = \
 """
 Checks all runs in the present working directory for timing problems. It does this by stripping
-out GPS time stamps, fitting a straight line to them and reporting if there any time not at the 
+out GPS time stamps, fitting a straight line to them and reporting if there any time not at the
 extreme ends that are further than expected off the fit.
 """
 
 import argparse, os, re, sys
+import numpy as np
+from scipy.optimize import leastsq
 from trm import ultracam
 
 parser = argparse.ArgumentParser(description=usage)
@@ -15,6 +17,8 @@ parser.add_argument('-s','--server',action="store_true",
                     help="get runs from server")
 parser.add_argument('-v','--verbose',action="store_true",
                     help="print more information")
+parser.add_argument('-b','--bad',action="store_true",
+                    help="report only identifiably bad frames")
 parser.add_argument('--absolute', '-a', type=float, default=0.0001,
                     help='absolute rejection threshold to eliminate bad times, units of days')
 parser.add_argument('--rms', '-r', type=float, default=5.,
@@ -46,49 +50,54 @@ for rpath, rnames, fnames in os.walk('.'):
             n   = np.array(n)
             gps = np.array(gps)
 
-            rej  = True
-            x    = [gps.mean(),0.0001]
-            ok   = n > -1
-            ind  = np.arange(len(ok),dtype=int)
-            while rej:
-                rej = False
-                res   = leastsq(func, x, (n[ok],gps[ok]))
-                x     = res[0]
-                d     = func(x,n[ok],gps[ok])
-                std   = d.std()
-                d     = np.abs(d)
-                if d.max() > args.absolute:
-                    if args.verbose: print 'Rejected time for frame',n[ok][d.argmax()]
-                    ok[ind[ok][d.argmax()]] = False
-                    rej = True
-                elif d.max() > args.rms*std:
-                    if args.verbose: print 'Rejected time for frame',n[ok][d.argmax()]
-                    ok[ind[ok][d.argmax()]] = False
-                    rej = True
+            if len(gps) > 2:
+                rej  = True
+                x    = [gps.mean(),0.0001]
+                ok   = n > -1
+                ind  = np.arange(len(ok),dtype=int)
+                while rej:
+                    rej = False
+                    res   = leastsq(func, x, (n[ok],gps[ok]))
+                    x     = res[0]
+                    d     = func(x,n[ok],gps[ok])
+                    std   = d.std()
+                    d     = np.abs(d)
+                    if d.max() > args.absolute:
+                        if args.verbose: print 'Rejected time for frame',n[ok][d.argmax()]
+                        ok[ind[ok][d.argmax()]] = False
+                        rej = True
+                    elif d.max() > args.rms*std:
+                        if args.verbose: print 'Rejected time for frame',n[ok][d.argmax()]
+                        ok[ind[ok][d.argmax()]] = False
+                        rej = True
 
-            bstart = 0
-            for val in ok:
-                if val:
-                    bstart += 1
-                else:
-                    break
+                bstart = 0
+                for val in ok:
+                    if not val:
+                        bstart += 1
+                    else:
+                        break
 
-            bend = len(ok)
-            for val in ok[::-1]:
-                if val:
-                    bend -= 1
-                else:
-                    break
+                bend = len(ok)
+                for val in ok[::-1]:
+                    if not val:
+                        bend -= 1
+                    else:
+                        break
 
-            if bstart < bend:
-                nbad = len(ok[bstart:bend][~ok[bstart:bend]])
-                if nbad:
-                    print run,'has',nbad,'bad times'
-                else:
-                    print run,'has',nbad,'bad times'
-            else:
-                print run,'has 0 bad times'
+                if bstart < bend:
+                    nbad = len(ok[bstart:bend][~ok[bstart:bend]])
+                    if nbad:
+                        print run,'has',nbad,'bad times'
+                    elif not args.bad:
+                        print run,'has',nbad,'bad times'
+                elif not args.bad:
+                    print run,'has 0 bad times'
+
+            cdelif not args.bad:
+                print run,'has',len(gps),'times total (< 3)'
 
         except Exception, err:
-            print run,'could not be read (probably a power on)'
-            print err
+            if args.verbose:
+                print run,'could not be read (probably a power on)'
+                print err
