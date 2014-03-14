b0d1c3752d4a1602c84cf95184299516760fad0e
Tom Marsh <t.r.marsh@warwick.ac.uk>
2014-03-14 08:33:43 +0000

    tchecker.py -- more options, supress warnings if not verbose

--- scripts/tchecker.py
+++ scripts/tchecker.py
@@ -17,13 +17,19 @@ parser.add_argument('-s','--server',action="store_true",
                     help="get runs from server")
 parser.add_argument('-v','--verbose',action="store_true",
                     help="print more information")
+parser.add_argument('--recursive','-r',action="store_true",
+                    help="run recursively")
 parser.add_argument('-b','--bad',action="store_true",
                     help="report only identifiably bad frames")
 parser.add_argument('--absolute', '-a', type=float, default=0.0001,
                     help='absolute rejection threshold to eliminate bad times, units of days')
-parser.add_argument('--rms', '-r', type=float, default=5.,
+parser.add_argument('--rms', type=float, default=5.,
                     help='RMS rejection threshold to eliminate bad times')
 args = parser.parse_args()
+
+if not args.verbose:
+    import warnings
+    warnings.filterwarnings("ignore")
 
 server = args.server
 
@@ -34,70 +40,79 @@ def func(x, n, gps):
     cad = x[1]
     return gps-t0-cad*n
 
-for rpath, rnames, fnames in os.walk('.'):
-    runs  = [fname[:-4] for fname in fnames if rmat.match(fname)]
-    runs.sort()
-    for run in runs:
-        try:
-            tdat  = ultracam.Rtime(run,server=server)
-            if args.verbose: print 'Starting on run',run
-            n   = []
-            gps = []
-            for nf, time in enumerate(tdat):
-                n.append(nf+1)
-                gps.append(time[1]['gps'])
+# Compile a list of runs
 
-            n   = np.array(n)
-            gps = np.array(gps)
+if args.recursive:
+    runs = []
+    for rpath, rnames, fnames in os.walk('.'):
+        runs.extend([os.path.join(rpath,fname[:-4]) for fname in fnames \
+                 if rmat.match(fname)] )
+else:
+    runs = [fname[:-4] for fname in os.listdir('.') \
+            if rmat.match(fname)]
+runs.sort()
 
-            if len(gps) > 2:
-                rej  = True
-                x    = [gps.mean(),0.0001]
-                ok   = n > -1
-                ind  = np.arange(len(ok),dtype=int)
-                while rej:
-                    rej = False
-                    res   = leastsq(func, x, (n[ok],gps[ok]))
-                    x     = res[0]
-                    d     = func(x,n[ok],gps[ok])
-                    std   = d.std()
-                    d     = np.abs(d)
-                    if d.max() > args.absolute:
-                        if args.verbose: print 'Rejected time for frame',n[ok][d.argmax()]
-                        ok[ind[ok][d.argmax()]] = False
-                        rej = True
-                    elif d.max() > args.rms*std:
-                        if args.verbose: print 'Rejected time for frame',n[ok][d.argmax()]
-                        ok[ind[ok][d.argmax()]] = False
-                        rej = True
+for run in runs:
+    try:
+        tdat  = ultracam.Rtime(run,server=server)
+        if args.verbose: print 'Starting on run',run
+        n   = []
+        gps = []
+        for nf, time in enumerate(tdat):
+            n.append(nf+1)
+            gps.append(time[1]['gps'])
 
-                bstart = 0
-                for val in ok:
-                    if not val:
-                        bstart += 1
-                    else:
-                        break
+        n   = np.array(n)
+        gps = np.array(gps)
 
-                bend = len(ok)
-                for val in ok[::-1]:
-                    if not val:
-                        bend -= 1
-                    else:
-                        break
+        if len(gps) > 2:
+            rej  = True
+            x    = [gps.mean(),0.0001]
+            ok   = n > -1
+            ind  = np.arange(len(ok),dtype=int)
+            while rej:
+                rej = False
+                res   = leastsq(func, x, (n[ok],gps[ok]))
+                x     = res[0]
+                d     = func(x,n[ok],gps[ok])
+                std   = d.std()
+                d     = np.abs(d)
+                if d.max() > args.absolute:
+                    if args.verbose: print 'Rejected time for frame',n[ok][d.argmax()]
+                    ok[ind[ok][d.argmax()]] = False
+                    rej = True
+                elif d.max() > args.rms*std:
+                    if args.verbose: print 'Rejected time for frame',n[ok][d.argmax()]
+                    ok[ind[ok][d.argmax()]] = False
+                    rej = True
 
-                if bstart < bend:
-                    nbad = len(ok[bstart:bend][~ok[bstart:bend]])
-                    if nbad:
-                        print run,'has',nbad,'bad times'
-                    elif not args.bad:
-                        print run,'has',nbad,'bad times'
+            bstart = 0
+            for val in ok:
+                if not val:
+                    bstart += 1
+                else:
+                    break
+
+            bend = len(ok)
+            for val in ok[::-1]:
+                if not val:
+                    bend -= 1
+                else:
+                    break
+
+            if bstart < bend:
+                nbad = len(ok[bstart:bend][~ok[bstart:bend]])
+                if nbad:
+                    print run,'has',nbad,'bad times'
                 elif not args.bad:
-                    print run,'has 0 bad times'
+                    print run,'has',nbad,'bad times'
+            elif not args.bad:
+                print run,'has 0 bad times'
 
-            cdelif not args.bad:
-                print run,'has',len(gps),'times total (< 3)'
+        elif not args.bad:
+            print run,'has',len(gps),'times total (< 3)'
 
-        except Exception, err:
-            if args.verbose:
-                print run,'could not be read (probably a power on)'
-                print err
+    except Exception, err:
+        if args.verbose:
+            print run,'could not be read (probably a power on)'
+            print err
